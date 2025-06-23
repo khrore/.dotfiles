3dd0c25ad6ff23554c12985f39d37de079d23b51
khrore <super.stas.0978@gmail.com>
2025-06-23 06:43:43 +0000

    lazyvim config created

---
+++ .config/nvim/lua/plugins/dap.lua
@@ -0,0 +1,76 @@
+-- LazyVim require some installing from meson (see line)
+-- but we dont want to use external package managers
+-- so we just rewrite js-ts-dap from https://www.lazyvim.org/extras/lang/typescript
+return {
+	"mfussenegger/nvim-dap",
+	dependencies = {
+		"mfussenegger/nvim-dap-python",
+    -- stylua: ignore
+    keys = {
+      { "<leader>dPt", function() require('dap-python').test_method() end, desc = "Debug Method", ft = "python" },
+      { "<leader>dPc", function() require('dap-python').test_class() end, desc = "Debug Class", ft = "python" },
+    },
+		config = function()
+			require("dap-python").setup("python")
+		end,
+	},
+	opts = function()
+		local dap = require("dap")
+		if not dap.adapters["pwa-node"] then
+			require("dap").adapters["pwa-node"] = {
+				type = "server",
+				host = "localhost",
+				port = "${port}",
+				executable = {
+					command = "node",
+					-- meson installing executable of js-debug as js-debug-adapter
+					-- this is the only reason to write this file, instead of just use LazyVim Extras buildin dap
+					args = {
+						"js-debug",
+						"${port}",
+					},
+				},
+			}
+		end
+		if not dap.adapters["node"] then
+			dap.adapters["node"] = function(cb, config)
+				if config.type == "node" then
+					config.type = "pwa-node"
+				end
+				local nativeAdapter = dap.adapters["pwa-node"]
+				if type(nativeAdapter) == "function" then
+					nativeAdapter(cb, config)
+				else
+					cb(nativeAdapter)
+				end
+			end
+		end
+
+		local js_filetypes = { "typescript", "javascript", "typescriptreact", "javascriptreact" }
+
+		local vscode = require("dap.ext.vscode")
+		vscode.type_to_filetypes["node"] = js_filetypes
+		vscode.type_to_filetypes["pwa-node"] = js_filetypes
+
+		for _, language in ipairs(js_filetypes) do
+			if not dap.configurations[language] then
+				dap.configurations[language] = {
+					{
+						type = "pwa-node",
+						request = "launch",
+						name = "Launch file",
+						program = "${file}",
+						cwd = "${workspaceFolder}",
+					},
+					{
+						type = "pwa-node",
+						request = "attach",
+						name = "Attach",
+						processId = require("dap.utils").pick_process,
+						cwd = "${workspaceFolder}",
+					},
+				}
+			end
+		end
+	end,
+}
