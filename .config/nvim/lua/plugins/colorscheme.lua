59f2998146807c6751125039bb3e6c55a00aa461
khrore <super.stas.0978@gmail.com>
2025-09-08 17:56:32 +0000

    fixed colorscheme nvim

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,7 @@
 return {
-	{
-		"LazyVim/LazyVim",
-		opts = {
-			colorscheme = "catppuccin",
-		},
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	"LazyVim/LazyVim",
+	opts = {
+		colorscheme = "catppuccin",
 	},
 }
