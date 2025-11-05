51ea839f259e2ed7664a65ae763edd592a6b1355
khrore <super.stas.0978@gmail.com>
2025-11-05 17:43:57 +0300

    fish and lazyvim

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -85,3 +85,10 @@ if test -s $DISPLAY
         end
     end
 end
+
+# >>> mamba initialize >>>
+# !! Contents within this block are managed by 'micromamba shell init' !!
+set -gx MAMBA_EXE "/home/user/.local/bin/micromamba"
+set -gx MAMBA_ROOT_PREFIX "/home/user/micromamba"
+$MAMBA_EXE shell hook --shell fish --root-prefix $MAMBA_ROOT_PREFIX | source
+# <<< mamba initialize <<<
